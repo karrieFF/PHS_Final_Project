bdf4d02fb885af5d63e3378d51cecda4f3fda7d8
Lingyi <fly.karrie@gmail.com>
2024-11-15 21:13:06 -0700

    update calculate the difference of effectiveness

--- Innovators.R
+++ Innovators.R
@@ -1,4 +1,16 @@
 # Function to calculate top innovators based on centrality
+ps_theory <- c(0.025, 0.135, 0.34, 0.34, 0.16)
+
+#the reason why we select 300 - 1500 is based on the theory, the increase for the total 60 days is 1500,
+#if we split them into 5 parts, that should be 12days, 24days, 36days, 48days, 60days.
+#the icnrease is relatively linear, the step increase for each time split should 300, 600, 900, 1200
+#people at the final stage may adopt the chatbot the shorted time
+#thus the increase of the steps at the final stage is 300 steps
+#if we hypothesis is not enirely linear
+effectivenss <- sort(runif(5, min = 300, max = 1500), decreasing  = TRUE) 
+non_adoption_efficacy <- -250 #finally
+
+
 get_top_innovators <- function(final_matrix, p_innovators, method) {
   # Calculate the number of innovators to select
   top_n <- round(nrow(final_matrix) * p_innovators)
@@ -53,7 +65,7 @@ get_top_innovators <- function(final_matrix, p_innovators, method) {
 
 # Example Usage
 # Replace final_matrix with your matrix
-p_innovators <- 0.025  # Proportion of innovators
+p_innovators <- ps_theory[1]  # Proportion of innovators
 method <- "closeness"  # Method: "in-degree", "betweeness", or "closeness"
 result <- get_top_innovators(final_matrix, p_innovators, method)
 
@@ -62,21 +74,11 @@ innovators_indice <- result$indices # Top indices
 innovators_values <- result$values   # Corresponding centrality values
 
 #effectiveness, adoption increase 1000 steps, non-adoption increase 250 steps
-non_adoption_efficacy <- -250
-adoption_efficacy <- 1500
 
-for (i in innovators_indice){
-  baseline_value <- original_data[i,'Baseline_PA']
-  follow_up_value <- baseline_value + adoption_efficacy
-  original_data[i,'Follow_up_PA'] <- follow_up_value
-}
-original_data
-
-
-
-
-
-
+adoption_efficacy <- effectivenss[1]
+original_data[i,'Follow_up_PA'] <- NaN
+  
+original_data[innovators_indice, 'Follow_up_PA'] <- original_data[innovators_indice, 'Baseline_PA'] + adoption_efficacy
 
 
 
--- adoption_rate.R
+++ adoption_rate.R
@@ -1,49 +1,61 @@
 
 #early adopter, the rest of 13.5% (DOI), adoption rate is 45% (prior knowledge)
 select <- c()
-p_theory <- 0.135
+#p_theory <- 0.135
+ps_theory <- c(0.025, 0.135, 0.34, 0.34, 0.16)
 p_prior <- 0.45
+stages <- seq(2:5)
 
-# Print the results
-print(result$indices)  # Top indices
-print(result$values)   # Corresponding centrality values
+effectivenss <- sort(runif(5, min = 300, max = 1500), decreasing  = TRUE) 
+non_adoption_efficacy <- -250 #finally
 
 n_total <- nrow(final_matrix)
-n_innovators <- length(result$indices)
-n_rest <- n_total - n_innovators
-
-#sort the linkage based on the same metric
-#if they select the similarity
 innovators <- result$indices
-select <- c(select, innovators)
+adoption_index <- c(innovators)
 
-# Initialize total data as an empty data frame
-total_data <- data.frame(Index = integer(), Value = numeric())
+for (stage in stages){
 
-for (i in 1:length(innovators)) {
-  index <- innovators[i]  # Select index
-  select_column <- final_matrix[, index]  # Select specific column
-  nonzero_indices <- which(select_column != 0, arr.ind = TRUE)  # Find indices of non-zero elements
-  filtered_indices <- nonzero_indices[!nonzero_indices %in% select]   # Exclude indices that have already been selected
-  nonzero_values <- select_column[filtered_indices]   # Get the corresponding values for filtered indices
+  n_rest <- n_total - length(adoption_index)
   
-  # Create a data frame for the current innovator's indices and values
-  new_data <- data.frame(Index = filtered_indices, Value = nonzero_values)   
+  # Initialize total data as an empty data frame
+  total_data <- data.frame(Index = integer(), Value = numeric())
   
-  if (nrow(new_data) > 0) { 
-    total_data <- merge(total_data, new_data, by = "Index", all = TRUE)  # Proceed only if there is new data # Merge with the cumulative total data
-    total_data$Value <- rowSums(total_data[, c("Value.x", "Value.y")], na.rm = TRUE)
-    total_data <- total_data[, c("Index", "Value")] # Remove intermediate columns and retain the final structure
+  for (i in 1:length(adoption_index)) {
+    index <- adoption_index[i]  # Select index
+    select_column <- final_matrix[, index]  # Select specific column
+    nonzero_indices <- which(select_column != 0, arr.ind = TRUE)  # Find indices of non-zero elements
+    filtered_indices <- nonzero_indices[!nonzero_indices %in% adoption_index]   # Exclude indices that have already been selected
+    nonzero_values <- select_column[filtered_indices]   # Get the corresponding values for filtered indices
+    
+    # Create a data frame for the current innovator's indices and values
+    new_data <- data.frame(Index = filtered_indices, Value = nonzero_values)   
+    
+    if (nrow(new_data) > 0) { 
+      total_data <- merge(total_data, new_data, by = "Index", all = TRUE)  # Proceed only if there is new data # Merge with the cumulative total data
+      total_data$Value <- rowSums(total_data[, c("Value.x", "Value.y")], na.rm = TRUE)
+      total_data <- total_data[, c("Index", "Value")] # Remove intermediate columns and retain the final structure
+    }
   }
+  
+  #theoretical number of adoption at the second stage based on DOI
+  num_adoption <- round(n_rest*ps_theory[stage]*p_prior)
+  
+  sorted_data <- total_data[order(-total_data$Value), ]
+  top_indices <- sorted_data$Index[1:num_adoption]
+  
+  adoption_index <- c(adoption_index, top_indices)
+  
+  #effectiveness at the second stage
+  adoption_efficacy <- effectivenss[stage]
+  original_data[top_indices, 'Follow_up_PA'] <- original_data[top_indices, 'Baseline_PA'] + adoption_efficacy
+  
 }
 
-#theoretical number of adoption at the second stage based on DOI
-num_adoption <- round(n_rest*p_theory*p_prior)
+length(adoption_index)
 
-sorted_data <- total_data[order(-total_data$Value), ]
-top_indices <- sorted_data$Index[1:num_adoption]
+non_adoption_index <- setdiff(id, adoption_index)
 
-select <- c(select, top_indices)
+#calculate the effectiveness of non_adoption
+original_data[non_adoption_index, 'Follow_up_PA'] <- original_data[non_adoption_index, 'Baseline_PA'] + non_adoption_efficacy
 
-#effectiveness adoption
-top_indices
+
